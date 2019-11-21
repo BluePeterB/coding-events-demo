9a477faec867ee15e0e98ea5f457146d30774f3f
Chris Bay <chris@chrisbay.net>
2019-11-21 09:34:39 -0600

    Implements template fragments

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -2,31 +2,50 @@ package org.launchcode.codingevents.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-/**
- * Created by Chris Bay
- */
 @Controller
 @RequestMapping("events")
 public class EventController {
 
     private static List<String> events = new ArrayList<>();
 
+    //lives at /events
     @GetMapping
+    //@ResponseBody  // debug only
     public String displayAllEvents(Model model) {
+        System.out.println("InGetMapNull");
+        //System.out.println(events.size());
         model.addAttribute("events", events);
         return "events/index";
     }
 
+    //lives at /events/create
     @GetMapping("create")
-    public String displayCreateEventForm() {
+    public String renderCreateEventForm() {
+        System.out.println("InGetMap");
+        return "events/create";
+    }
+
+        @PostMapping("create")
+        public String createEvent (@RequestParam String eventName){
+            System.out.println("InPostMap");
+            events.add(eventName);
+            //System.out.println(events);
+            return "redirect:";
+        }
+    }
+
+
+  /*
+    //lives at /events/create
+    @GetMapping("create")
+    public String displayCreateEventForm(Model model) {
+        model.addAttribute("title", "Create Event");
         return "events/create";
     }
 
@@ -36,4 +55,5 @@ public class EventController {
         return "redirect:";
     }
 
-}
+   */
+
